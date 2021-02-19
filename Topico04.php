42109a31af6724459c0b698d5078a3564927ab6e
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-02-19 14:56:56 -0300

    Update Topico04.php

--- Topico04.php
+++ Topico04.php
@@ -41,6 +41,7 @@ include('layout/header.php');
                     Atualmente, os países componentes da OCDE são: Alemanha, Austrália, Áustria, Bélgica, Canadá, Chile, Coreia, Dinamarca, Espanha, Estados Unidos, Eslovênia, Estônia, Finlândia, França, Grécia, Hungria, Irlanda, Islândia, Israel, Itália, Japão, Letônia, Luxemburgo, México, Noruega, Nova Zelândia, Países Baixos, Polônia, Portugal, Reino Unido, República Checa, Suécia, Suíça e Turquia. O Brasil está categorizado como país-chave em termos de colaboração global (<a href="https://www.oecd.org/">www.oecd.org</a>)
                 </p>
             </div>
+            <br>
 
             <p class="Texto">
                 O conselho da OCDE percebeu a gravidade desse problema e reagiu analisando as possíveis causas que provocavam essa desconfiança. Em abril de 1998, a Junta do Conselho solicitou que fosse elaborado um informativo nos países membros sobre a conduta ética no serviço público e a prevenção da corrupção. O órgão encarregado de cumprir essa tarefa foi o Comitê de Gestão Pública da OCDE (em inglês, PUMA). Dois anos depois, nos dias 26 e 27 de junho de 2000, foi apresentado o relatório final da Junta do Conselho da OCDE.
@@ -50,9 +51,153 @@ include('layout/header.php');
             </p>
 
             <div>
-                    <p class="SaibaMaisTitulo"> Saiba Mais</p>
-                    <p class="SaibaMaisTexto">Obenha mais informações com a leitura do documento OECD, Trust in Government: ethics measures in OECD countries, Washington: OECD, 2000.</p>
+                <p class="SaibaMaisTitulo"> Saiba Mais</p>
+                <p class="SaibaMaisTexto">Obenha mais informações com a leitura do documento OECD, Trust in Government: ethics measures in OECD countries, Washington: OECD, 2000.</p>
+            </div>
+            <br>
+
+            <p class="Texto">
+                A ética tornou-se, assim, um importante assunto de interesse público que refletiu na variedade de tentativas de “limpar” a vida pública, incluindo a revisão dos sistemas de gestão e comportamento dos funcionários. Tal processo de limpeza, naturalmente, exige uma combinação de diversos mecanismos e agentes desse processo, que precisam estar interrelacionados, a fim de garantir o seu êxito. Ou seja, o PUMA reconhece que a prevenção da corrupção é tão complexa quanto a própria corrupção. De forma que se faz necessário tanto identificar o corrupto como também “os mecanismos e as estruturas de ação que conduzem à corrupção”.
+            </p>
+            <p class="Texto">
+                A proposta de infraestrutura ética da OCDE integra oito componentes: o compromisso político, o marco legal, os mecanismos de responsabilidade, os códigos de conduta, a socialização profissional, as condições do serviço público, os órgãos de coordenação e participação e votações públicas. Por sua vez, estes componentes são agrupados em outras três funções principais: Controle, Orientação e Gestão.
+            </p>
+
+            <section id="Controle">
+                <div class="accordion" id="accordionExample">
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                            <h2 class="mb-0">
+                                <button class="btn btn-success btn-block text-left collapsed " type="button" data-toggle="collapse" data-target="#collapse1" aria-expanded="false" aria-controls="collapse1">
+                                    <h3>Controle</h3>
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapse1" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div class="card-body">
+                                <p class="Texto">
+                                    O Controle é possível de ser alcançado mediante algumas iniciativas como, por exemplo, os seguintes componentes:
+                                </p>
+                                <ul class="fa-ul">
+                                    <li>
+                                        <p class="Texto">
+                                            <span class="fa-li"><i class="fas fa-caret-right" style="color: #FC6B30;"></i></span><strong> Um marco legal que permita a investigação e acusação independentes:</strong>
+                                        </p>
+                                        <p class="Texto">
+                                            Um marco legal está relacionado com a garantia de que a ética seja vigente e, em caso de não haver ainda sido criado, buscar fazêlo, como alguns países têm procedido.
+                                        </p>
+                                        <div class="row">
+                                            <div class="col-1 center border" style="background-color:#4885AE;">
+                                                <p class="Texto" style="color:white;">
+                                                    Exemplo
+                                                </p>
+                                            </div>
+                                            <div class="col-sm center border">
+                                                <p class="Texto">
+                                                    No Japão, a lei sobre a ética dos servidores públicos (1999) exige que o gabinete estabeleça um novo código de conduta para proibir que os servidores públicos recebam presentes por parte de companhias privadas que estejam sob sua jurisdição. Por seu turno, a Polônia adotou uma lei que exige que todos os funcionários públicos declarem seus ativos financeiros, propriedades e capital de negócios.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <br>
+                                    <br>
+                                    <li>
+                                        <p class="Texto">
+                                            <span class="fa=li"><i class="fas fa-caret-right" style="color: #FC6B30;"></i></span><strong> Mecanismos adequados de responsabilidade e participação:</strong>
+                                        </p>
+                                        <p class="Texto">
+                                            Os mecanismos de prestação de contas devem estar alertas, a fim de evitar ações antiéticas e cortar o seu fluxo de ação. Entre os mecanismos, para além do procedimento legal, podemos citar: o procedimento administrativo, as auditorias, as avaliações de desempenho. Outros mecanismos podem ser externos à organização como, por exemplo, a vigilância de comitês parlamentares. Um outro fator importante e imprescindível é a participação cidadã. As denúncias da população são a melhor garantia de que existe uma vigilância e acompanhamento da conduta dos funcionários.
+                                        </p>
+                                    </li>
+                                    <br>
+                                    <li>
+                                        <p class="Texto">
+                                            <span class="fa=li"><i class="fas fa-caret-right" style="color: #FC6B30;"></i></span><strong> Votações públicas:</strong>
+                                        </p>
+                                        <p class="Texto">
+                                        A participação e a votação pública são poderosos fatores desanimadores da corrupção e das condutas indevidas no setor público. Entretanto, estes dependem, em grande medida, de que o marco legal permita o acesso à informação.
+                                        </p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </section>
+            <br>
+
+            <section id="Orientacao">
+                <div class="accordion" id="accordionExample">
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                            <h2 class="mb-0">
+                                <button class="btn btn-success btn-block text-left collapsed " type="button" data-toggle="collapse" data-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
+                                    <h3>Orientação</h3>
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapse2" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                            <div class="card-body">
+                            <p class="Texto">
+                                Os seguintes elementos podem auxiliar na boa consecução da orientação:
+                            </p>
+                            <ul class="fa-ul">
+                                <li>
+                                    <p class="Texto">
+                                        <span class="fa-li"><i class="fas fa-caret-right" style="color: #FC6B30;"></i></span><strong> Compromisso bem articulado dos líderes políticos:</strong>
+                                    </p>
+                                    <p class="Texto">
+                                        Os esforços para fomentar a ética no serviço público podem ser totalmente em vão e inútil sem o apoio e compromisso político firme. As tentativas para melhorar a ética no setor público nos países da OCDE emanam dos níveis políticos mais altos. As lideranças políticas desempenham papéis muito importantes nessa questão e deveriam ser um exemplo positivo.</p>
+                                </li>
+                                <br>
+                                <li>
+                                    <p class="Texto">
+                                        <span class="fa-li"><i class="fas fa-caret-right" style="color: #FC6B30;"></i></span><strong> Códigos de conduta que determinem que os valores e normas sejam aplicáveis:</strong>
+                                    </p>
+                                    <p class="Texto">
+                                        Os códigos de conduta possuem um papel vital no estabelecimento das normas de comportamento esperadas, particularmente, nos governos (mais em particular ainda essa força normativa está presente nos países componentes da OCDE). Alguns países aplicam um código de conduta geral para o serviço público e, em outros casos, os códigos são estabelecidos para cada organismo. </p>
+                                </li>
+                                <br>
+                                <li>
+                                    <p class="Texto">
+                                        <span class="fa-li"><i class="fas fa-caret-right" style="color: #FC6B30;"></i></span><strong> Mecanismos ou atividades de socialização profissionais tais como a educação e formação:</strong>
+                                    </p>
+                                    <p class="Texto">
+                                        O conteúdo dos códigos de conduta pode tornar-se incipiente caso não se comunique e seja introspectivo de maneira adequada. Tal capacitação será chave para que os servidores aprimorem sua consciência no desenvolvimento dos dilemas éticos.</p>
+                                </li>
+                            </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <br>
+
+            <section id="Gestao">
+                <div class="accordion" id="accordionExample">
+                    <div class="card">
+                        <div class="card-header" id="headingThree">
+                            <h2 class="mb-0">
+                                <button class="btn btn-success btn-block text-left collapsed " type="button" data-toggle="collapse" data-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
+                                    <h3>Gestão</h3>
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapse3" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                            <div class="card-body">
+                            <p class="Texto">
+                                Sobre gestão, podemos citar o seguinte:
+                            </p>
+                            <p class="Texto">
+                                A nova gestão pública resgata e introduz uma série de novos valores. Não se pode mais considerar a administração apenas como umas simples estrutura e instrumento executor de ordens políticas, mas se reorienta a atividade administrativa para a consecução de resultados, em direção a uma cultura com qualidade de serviço, a maiores níveis de autonomia e de responsabilidade dos gestores e servidores. Essa nova situação apresenta o papel da cultura organizacional e da ética pública como instrumentos de controle das burocracias políticas.
+                            </p>
+                            <p class="Texto">
+                                Em certo sentido, a administração pública repousa, por um lado, nos princípios do modelo racional weberiano (burocracia), ou seja, é dentro do “paradigma weberiano que a discussão da ética se efetiva, uma discussão própria à racionalidade da vida moderna”. E, por outro, na incorporação de novos valores representados pela realização prática de ações eficazes e eficientes que possam responder às necessidades sociais de forma satisfatória, com a qualidade adequada e, ainda, com o menor custo possível.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
             </div> <!-- CLASS PAGE TITLE FIM -->
             
