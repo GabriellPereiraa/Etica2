cd1b4f3bc5db11ddc35afc8f701b0bf108fbf467
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-02-19 16:34:55 -0300

    Create Topico04.7.php

---
+++ Topico04.7.php
@@ -0,0 +1,67 @@
+<?php
+// HEADER
+include('layout/header.php');
+?>
+
+<body>
+    <!-- PLUGIN DO FADE IN -->
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+
+    <!-- MENU -->
+    <?php include('layout/menu.php') ?>
+
+    <div id="main">
+        <!-- NAV TOP -->
+        <?php include('layout/nav.php') ?>
+        <!-- CONTEUDO INICIO -->
+        <div class="main-content container-fluid">
+            <h2 class="font-bold Azul">
+                <i class="fas fa-folder"></i> 04.6 - Outras experiências estrangeiras
+            </h2>
+
+            <hr class="hr4">
+            <p class="Texto">
+            O problema da corrupção na América Latina tem se acentuado de uma maneira impressionante nas últimas décadas. Essa situação tem motivado uma série de iniciativas para conter esse problema. Uma dessas tentativas tem sido buscar fomentar a ética pública. Destacaremos, a seguir, dois projetos, a título de exemplo. Tais projetos merecem relevo dada a magnitude de seu alcance e também a sua capacidade de inovação.
+            </p>
+            <br>
+            <hr>
+            <h4 class="font-bold AzulEstado"><i class="fas fa-angle-double-right"></i> 04.7.1 - Rede de Instituições de Combate à Corrupção e Resgate da Ética Pública</h4>
+            <hr class="hrAEstado">
+            <p class="Texto">
+                Nos dias 15 e 16 de junho de 1998, o <span style="color:#FC6B30;">Centro Latino-americano de Administração para o Desenvolvimento (</span>CLAD – Centro Latinoamericano de Administración para el Desarrollo<span style="color:#FC6B30;">)</span>, com o apoio da <span style="color:#FC6B30;">Agência Espanhola de Cooperação Internacional para o Desenvolvimento (</span>(AECID - Agencia Española de Cooperación Internacional para el Desarrollo<span style="color:#FC6B30;">)</span>, realizou em Santa Cruz de la Sierra, na Bolívia, um Fórum Ibero-americano com o propósito de: examinar as formas mais proeminentes e as consequências sociais da corrupção, e de explorar as formas e métodos que atualmente têm sido adotados no mundo para combatê-la e, dessa maneira, buscar reduzir sua presença e atividade na região. Um dos resultados desse fórum foi a criação da Rede de Instituições de Combate à Corrupção e Resgate da Ética Pública
+            </p>
+            <p class="Texto">
+                – RICOREP.
+            </p>
+            <p class="Texto">
+                A RICOREP reúne diversas instituições governamentais e nãogovernamentais (instituições de investigação com sede nos Estados Unidos e na América Latina), que possuem as funções de combater a corrupção em todas as suas manifestações e de resgatar os princípios éticos no exercício da função pública. Essa rede está formada por pessoas que desenvolvem estudos e investigações sobre essa temática. Entre os seus objetivos, incluem-se: o intercâmbio de informações sobre a estrutura e organização de cada uma das instituições referidas e as normas vigentes em matéria de enfrentamento da corrupção. Entre outras atividades, promove eventos periodicamente que estejam relacionados a essa matéria, realiza atividades de formação, investigação, consultoria técnica e difusão de publicações especializadas.
+            </p>
+            <p class="Texto">
+            São considerados “Membros Plenos” da RICOREP: as instituições cuja missão e objetivos estejam referidos à luta contra qualquer tipo de corrupção e ao resgate da ética pública. São considerados “Membros Pessoais”: os professores, pesquisadores, assessores e consultores sobre o tema em questão. A Secretaria Geral da CLAD é membro de pleno direito da RICOREP, em seu caráter de instância coordenadora das redes do Centro. Os países membros que integram a Rede são: Argentina, Bolívia, Brasil, Chile, Colômbia, Costa Rica, Cuba, Equador, El Salvador, Espanha, Estados Unidos, Guatemala, Honduras, Índia, México, Nicarágua, Panamá, Paraguai, Peru, Portugal, Porto Rico, República Dominicana, Uruguai e Venezuela.
+            </p>
+            </div> <!-- CLASS PAGE TITLE FIM -->
+            
+            <br><br>
+            <div class="center">
+                <div class="btn-group" id="btn-group">
+                    <a href="Topico04.6.php" class="btn btn-outline-success btn-sm">Página Anterior</a>
+                    <a href="Topico04.7.2.php" class="btn btn-outline-success btn-sm">Proxima Página</a>
+                </div>
+            </div>
+
+        </div> <!-- CONTEUDO FIM -->
+
+        <!-- FOOTER -->
+        <?php include('layout/footer.php') ?>
+
+    </div> <!-- DIV MAIN FIM -->
+
+    <!-- FOOTER JS -->
+    <?php include('layout/js.php') ?>
+
+</body>
+
+</html>
