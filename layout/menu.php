ebaecd8ae8c4c81b3463a766b7fe7056e9befd79
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-02-19 11:46:43 -0300

    Start

---
+++ layout/menu.php
@@ -0,0 +1,58 @@
+<body>
+    <div id="app">
+        <div id="sidebar">
+            <div class="sidebar-wrapper active ps ps--active-y">
+                <div class="m-4">
+                    <a href="index.php">
+                        <img class="img-fluid" src="http://ava.egp.ce.gov.br/2018/desv/logo/egp.png" alt="Logo EGPCE" srcset="">
+                    </a>
+                </div>
+                <div class="sidebar-menu ">
+                    <ul class="menu">
+                        <!--  <li class="sidebar-item">
+                        <a href="../Apresentacao.php" class="sidebar-link">
+                            <i data-feather="home" width="20"></i>
+                            <span>Apresentação</span>
+                        </a>
+                    </li> -->
+                        <li class="sidebar-item">
+                            <a href="Apresentacao.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>Apresentação</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="Topico01.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>1.0 - Introdução à Ética</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="Topico02.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>2.0 - Conceitos básicos</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="Topico03.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>3.0 - Concepções éticas</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="Topico04.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>4.0 - Visão Contemporânea</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="Referencias.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>Referências</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+            </div>
+        </div>
