f4ec46d8c477d714e5ee7ef452a8c7be9c726433
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-02-19 16:27:19 -0300

    Add new color in UX + Add Text in Topico4.6

--- Topico04.6.php
+++ Topico04.6.php
@@ -113,6 +113,20 @@ include('layout/header.php');
                     </div>
                 </div>
             </section>
+            <br>
+            <hr>
+            <h4 class="font-bold AzulEstado"><i class="fas fa-angle-double-right"></i> 04.6.1 - O caso australiano</h4>
+            <hr class="hrAEstado">
+            <p class="Texto">
+                Os servidores públicos australianos contam com uma série de documentos oficiais que estabelecem os padrões de conduta ética no serviço. Os valores éticos fundamentais que devem ser afirmados pelos servidores públicos australianos são: pronto atendimento às necessidades do governo; concentração nos resultados; mérito como base para o emprego; altos padrões de probidade, integridade e conduta; forte compromisso com a responsabilidade e a prestação de contas; melhoria contínua das equipes e dos indivíduos.
+            </p>
+            <br>
+            <hr>
+            <h4 class="font-bold AzulEstado"><i class="fas fa-angle-double-right"></i> 04.6.2 - O caso holandês</h4>
+            <hr class="hrAEstado">
+            <p class="Texto">
+                Na Holanda, o problema da ética no serviço público impôs-se a partir de uma preocupação com a integridade (ou seja, incorruptibilidade, correção e confiabilidade) da administração pública, após uma série de incidentes que envolviam vazamento de informações, transações duvidosas e rumores na imprensa. Essa preocupação com a integridade do serviço público está diretamente ligada ao receio de perda de confiança por parte dos cidadãos, sem a qual a democracia não poderia funcionar. Não por acaso, a questão principal para o governo holandês não é tanto a fraude ou a corrupção em si mesmas, mas o abuso de poder implicado por essas e outras práticas antiéticas e ilegais. Para garantir a integridade de seu serviço público, o governo holandês conta com uma infraestrutura que engloba instrumentos legais, órgãos especiais responsáveis pela ética no serviço público como um todo, mecanismos de prestação de contas e controle interno, códigos de conduta e processos de educação e formação.
+            </p>
             </div> <!-- CLASS PAGE TITLE FIM -->
             
             <br><br>
